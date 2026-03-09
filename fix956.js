b6a3ba8d9b4cb24c8667b4f4bd9229c047da16fa
Mireia <mireia.castillo@idiada.com>
2026-03-09 11:50:44 +0100

    fix

--- fix956.js
+++ fix956.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
+const file = 'extensions/jupydrive-s3/static/956.698b70cd333da8a54756.js';
+let c = fs.readFileSync(file, 'utf8');
 
-// Rename the JS file with a new hash
-const oldFile = 'extensions/jupydrive-s3/static/956.698b70cd333da8a54755.js';
-const newHash = '698b70cd333da8a54756'; // increment last digit
-const newFile = `extensions/jupydrive-s3/static/956.${newHash}.js`;
-fs.copyFileSync(oldFile, newFile);
+// Fix: change 6 closing braces to 7
+c = c.replace(
+  'secretAccessKey:""}}}}}}))),',
+  'secretAccessKey:""}}}}}}})),'
+);
 
-// Update remoteEntry to point to new hash
-const remoteFile = 'extensions/jupydrive-s3/static/remoteEntry.20b5a0279ea394f88afa.js';
-let r = fs.readFileSync(remoteFile, 'utf8');
-r = r.replace(/956:"698b70cd333da8a54755"/g, `956:"${newHash}"`);
-r = r.replace(/698b70cd333da8a54755/g, newHash);
-fs.writeFileSync(remoteFile, r, 'utf8');
-
-console.log('Done. New file:', newFile);
-console.log('remoteEntry updated:', r.includes(newHash));
+fs.writeFileSync(file, c, 'utf8');
+console.log('Fixed. Verify:', c.includes('secretAccessKey:""}}}}}}})),'));
